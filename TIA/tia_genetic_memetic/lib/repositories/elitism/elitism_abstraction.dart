bf9ec2ba8e33422f06a294ac737b4a4a8770c6f8
Crdzbird <luisalfonsocb83@gmail.com>
2023-10-21 21:55:43 +0200

    Updates

--- TIA/tia_genetic_memetic/lib/repositories/elitism/elitism_abstraction.dart
+++
@@ -1,5 +0,0 @@
-import 'package:tia_genetic_memetic/repositories/individual/individual_abstraction.dart';
-
-abstract class ElitismInterface<I extends IndividualInterface> {
-  List<I> elitism(List<I> individuals, bool accepted);
-}
--- TIA/tia_genetic_memetic/lib/repositories/population/population_abstraction.dart
+++
@@ -1,9 +0,0 @@
-import 'package:tia_genetic_memetic/repositories/individual/individual_abstraction.dart';
-
-abstract class PopulationInterface<I extends IndividualInterface> {
-  void evolve(bool shouldEvolve);
-  void sort();
-  I retrieveBestIndividual();
-  PopulationInterface<I> copy();
-  List<I> getMembers();
-}
